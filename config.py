ab328ea762d8ab9882a162a89d7442d78600777d
nsazhi <n190275@inbox.ru>
2025-01-18 17:07:10 +0400

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-from starlette.config import Config
-
-config = Config('.env')
-
-SECRET_KEY = config('SECRET_KEY', default='you-will-never-guess')
-SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='sqlite:///./ mff.db')
