5115efcc55ab5c3080437856dc3af9a6453e6fbf
nsazhi <n190275@inbox.ru>
2025-01-18 17:06:21 +0400

    Delete mff_app directory

--- mff_app/schemas/__init__.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-from datetime import date
-
-from .category import CreateCategory
-from .films import CreateFilm
-from .admin import CreateAdmin
--- mff_app/schemas/admin.py
+++
@@ -1,9 +0,0 @@
-from mff_app.schemas import *
-
-
-class CreateAdmin(BaseModel):
-    """
-    Валидирует входящие данные и создает объект Администратор.
-    """
-    username: str = Field(..., min_length=3, description='Логин админа')
-    password: str = Field(..., description='Пароль админа')
